397ea4e31023f33415b85b7edc25ec3598d609ee
Orion / Emilien <contact@oriondev.fr>
2025-09-06 17:41:25 +0200

    add lot of flags

--- src/data/dataset.go
+++ src/data/dataset.go
@@ -25,4 +25,68 @@ var DATASET = []Data{
 		Name: "Agender",
 		File: "agender.png",
 	},
+	{
+		Name: "Lesbian",
+		File: "lesbian.png",
+	},
+	{
+		Name: "Pansexual",
+		File: "pansexual.png",
+	},
+	{
+		Name: "Omnisexaul",
+		File: "omnisexual.png",
+	},
+	{
+		Name: "Demiboy",
+		File: "demiboy.png",
+	},
+	{
+		Name: "Demigirl",
+		File: "demigirl.png",
+	},
+	{
+		Name: "Intersex",
+		File: "intersex.png",
+	},
+	{
+		Name: "Aromantic",
+		File: "aromantic.png",
+	},
+	{
+		Name: "Asexual",
+		File: "asexual.png",
+	},
+	{
+		Name: "bigender",
+		File: "bigender.png",
+	},
+	{
+		Name: "Genderfluid",
+		File: "genderfluid.png",
+	},
+	{
+		Name: "Genderqueer",
+		File: "genderqueer.png",
+	},
+	{
+		Name: "Neurogender",
+		File: "neurogender.webp",
+	},
+	{
+		Name: "Neutrois",
+		File: "neutrois.webp",
+	},
+	{
+		Name: "Trigender",
+		File: "trigender.svg",
+	},
+	{
+		Name: "Pangender",
+		File: "pangender.png",
+	},
+	{
+		Name: "Polysexual",
+		File: "polysexual.png",
+	},
 }
